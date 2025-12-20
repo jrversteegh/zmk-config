1acad212ef1e76e3052088365ed15f1b85ee8f93
Jaap Versteegh <j.r.versteegh@gmail.com>
2025-12-20 15:09:26 +0100

    Make display stay off after being put to sleep

--- boards/shields/nice_view_nano_adapter/nice_view_nano_adapter.c
+++ boards/shields/nice_view_nano_adapter/nice_view_nano_adapter.c
@@ -18,30 +18,46 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define DPP DT_NODELABEL(reg_display_power)
 static struct gpio_dt_spec const dpp_spec = GPIO_DT_SPEC_GET(DPP, enable_gpios);
 
+static bool permanently_off = false;
+
+static int switch_on() {
+    if (permanently_off) {
+        LOG_INF("Not switching on display: permanently off");
+        return 0;
+    }
+    int ret = gpio_pin_set_dt(&dpp_spec, 1);
+    if (ret == 0) {
+        LOG_INF("Set display power pin");
+    }
+    else {
+        LOG_ERR("Failed to set display power pin");
+    }
+    return ret;
+}
+
+static int switch_off() {
+    int ret = gpio_pin_set_dt(&dpp_spec, 0);
+    if (ret == 0) {
+        LOG_INF("Cleared display power pin");
+    }
+    else {
+        LOG_ERR("Failed to clear display power pin");
+    }
+    return ret;
+}
+
 static int nice_view_nano_adapter_event_listener(const zmk_event_t *eh) {
     int ret = 0;
     const struct zmk_activity_state_changed *eva = as_zmk_activity_state_changed(eh);
     if (eva) {
         switch (eva->state) {
         case ZMK_ACTIVITY_ACTIVE:
-            ret = gpio_pin_set_dt(&dpp_spec, 1);
-            if (ret == 0) {
-              LOG_INF("Set display power pin");
-            }
-            else {
-              LOG_ERR("Failed to set display power pin");
-            }
-            return ret;
+            return switch_on();
         case ZMK_ACTIVITY_IDLE:
+            return switch_off();
         case ZMK_ACTIVITY_SLEEP:
-            ret = gpio_pin_set_dt(&dpp_spec, 0);
-            if (ret == 0) {
-              LOG_INF("Cleared display power pin");
-            }
-            else {
-              LOG_ERR("Failed to clear display power pin");
-            }
-            return ret;
+            permanently_off = true;
+            return switch_off();
         default:
             break;
         }
